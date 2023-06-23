6445dcd1b31c1aa81ebd157fc8cbe93972086b57
Dimitri Ribeiro <dimitriiff00@gmail.com>
2023-06-23 11:31:08 -0300

    implementando a superclasse Funcionario e a subclasse Gerente

---
+++ src/entidades/Gerente.java
@@ -0,0 +1,48 @@
+package entidades;
+
+public class Gerente extends Funcionario {
+
+	private int senha;
+	private int numeroDeFuncionariosGerenciados;
+
+	public Gerente(String nome, String cpf, double salario) {
+		super(nome, cpf, salario);
+	}
+
+	public Gerente(String nome, String cpf, double salario, int senha, int numeroDeFuncionariosGerenciados) {
+		super(nome, cpf, salario);
+		this.senha = senha;
+		this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+	}
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public int getNumeroDeFuncionariosGerenciados() {
+		return numeroDeFuncionariosGerenciados;
+	}
+
+	public void setNumeroDeFuncionariosGerenciados(int numeroDeFuncionariosGerenciados) {
+		this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+	}
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			System.out.println("Acesso Permitido!");
+			return true;
+		} else {
+			System.out.println("Acesso Negado!");
+			return false;
+		}
+	}
+	@Override
+	public double getBonificacao() {
+		return salario * 0.10;
+	}
+
+}
